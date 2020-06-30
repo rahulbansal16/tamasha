6cfcf7b0bebbbd250efab6d4f2c9a5ee66199a97
Rahul Bansal <bansal.rahul14@gmail.com>
2020-06-30 16:58:58 +0530

    added a component for automatic login

---
+++ src/component/RedirectToLogin.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {withRouter} from "react-router-dom";
+import {UserContext} from '../UserProvider';
+
+class RedirectToLogin extends React.Component {
+    static contextType = UserContext;
+    render(){
+        if (!this.context){
+            this.props.history.push({
+                pathname:"/login",
+                successUrl: this.props.location
+            });
+        } 
+       return(<></>);
+    }
+  
+}
+
+export default withRouter(RedirectToLogin);
