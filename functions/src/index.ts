58294860ba7be9b0cfeed94488cb11aa84e2b3c8
Rahul Bansal <bansal.rahul14@gmail.com>
2020-07-01 20:28:55 +0530

    added api for creating event

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as firebaseHelper from 'firebase-functions-helper';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
 import { user } from 'firebase-functions/lib/providers/auth';
+import {fetchUserIdFromRequest} from './util/util';
+
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 const app = express();
@@ -26,23 +28,42 @@ app.get('/contacts', (req, res) => {
         .catch(error => res.status(400).send(`Cannot get contacts: ${error}`));
 })
 
+
 // TODO: Add authentication before creating the event
 app.post('/event', (req, res) => {
-    admin.firestore().collection('event').add({})
-    .then()
-    .catch()
+
+    const event:any = {
+        name: req.body.name,
+        description: req.body.description,
+        creator: fetchUserIdFromRequest(req),
+        videoStreamUrl: 'https://www.youtube.com',
+        eventDate: new Date()
+    }
+
+    console.log("Post: /event creation request", event);
+    admin.firestore().collection('events').add(event)
+    .then( snap => {
+        console.log("The document was written successfully");
+        res.send({
+            id: snap.id,
+            success: true
+        })
+    })
+    .catch( () => {
+        console.log("Error creating an event");
+        res.send({
+            success:false,
+            error:'Unable to create an Event'
+        })
+    })
 })
 
 // TODO: Add authentication so that only creator can access the 
 // Fetches all the registered users
 app.get('/event/:id', (req, res) => {
-    // Figure out the logic to add the user in the par
-    // res.send(req.params.id);    
-    console.log("Hitting the event API");
-    admin.firestore().doc('eventRegistration/' + req.params.id).get()
+    admin.firestore().doc('events/' + req.params.id).get()
     .then(snap => res.send(snap.data()))
     .catch(() => res.send("failed"))
-    // res.send({id: req.params.id})
 })
 
 // TODO: Add user authentication
@@ -57,6 +78,51 @@ app.post('/event/:id/register', (req, res) => {
     .then(() => res.send("done"))
     .catch(() => res.send("oops"))
 })
+
+// API to push questions to the events
+
+app.post('/event/:id/question/:number', (req, res) => {
+
+    console.log("Hitting the API to push data to the live questions")
+    // Check if the user has access to the mentioned event or not
+
+    const eventId = req.params.id;
+    const questionNumber = req.params.number
+
+    if ( !eventId || !questionNumber){
+        res.send({
+            error: "EventId and Question Number are required",
+            success: false
+        })
+    }
+    admin.firestore()
+    .doc('eventQuestion/' + eventId +'/questions/'+ questionNumber)
+    .get()
+    .then( snap => {
+        const {question, id, options}: any = snap.data();
+        admin.firestore()
+        .doc('liveQuestions/' + eventId)
+        .update({
+            question: question,
+            id: id,
+            options: options
+        })
+        .then(() => res.send({
+            success: true
+        }))
+        .catch(() => res.send({
+            error: "Unable to update the liveQuestion",
+            success: false
+        }))
+    })
+    .catch(() => {
+        console.log("Unable to update the questions")
+        res.send({
+            error:'Unable to update the Questions',
+            success: false
+        })
+    })
+});
 
 app.get('/event/:eventID/question/:questionID/submit', (req, res) => {
 
