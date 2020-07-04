7e7805ac63fc5538b6975c038cb5ca4103bfd119
Rahul Bansal <bansal.rahul14@gmail.com>
2020-07-04 12:35:10 +0530

    added fadeInUp animation

--- src/component/AppHeader.js
+++ src/component/AppHeader.js
@@ -7,10 +7,10 @@ class AppHeader extends React.Component {
 
     render(){
         return (
-            <>
-                <Header as='h1' content = "Tamasha" style = {{marginBottom:'0px'}}></Header>
-                <Header as= 'h3' content = "Play, Win and Earn" style = {{marginTop:'0px'}}></Header>
-            </>
+            <div className ="fadeInUp">
+                    <Header as='h1' content = "Tamasha" style = {{marginBottom:'0px'}}></Header>
+                    <Header as= 'h3' content = "Play, Win and Earn" style = {{marginTop:'0px'}}></Header>
+            </div >
         );
     }
 }
