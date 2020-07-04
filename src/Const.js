5a75b4b5f31e64d858615e5f07ad486645372349
Rahul Bansal <bansal.rahul14@gmail.com>
2020-07-04 12:34:45 +0530

    created constants file

---
+++ src/Const.js
@@ -0,0 +1,10 @@
+export const EventStatus = {
+    LIVE: 'live',
+    UPCOMING: 'upcoming',
+    CANCELLED: 'cancelled'
+};
+
+export const PaymentStatus = {
+    PENDING: 'pending',
+    RECEIVED: 'received'
+}
